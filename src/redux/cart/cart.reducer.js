1268b5b5137705de3563c9ebf1580590e1beb090
Chinedu Oguejiofor <=>
2019-10-31 11:49:23 +0100

    Created Add and remove feature,checkout completed

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -13,7 +13,8 @@ const INITIAL_STATE = {
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
+  const { type, payload } = action;
+  switch (type) {
     case TOGGLE_CART_HIDDEN:
       return {
         ...state,
@@ -22,18 +23,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case ADD_ITEM:
       return {
         ...state,
-        cartItems: addItemToCart(state.cartItems, action.payload)
+        cartItems: addItemToCart(state.cartItems, payload)
       };
     case REMOVE_ITEM:
       return {
         ...state,
-        cartItems: removeItemFromCart(state.cartItems, action.payload)
+        cartItems: removeItemFromCart(state.cartItems, payload)
       };
     case CLEAR_ITEM_FROM_CART:
       return {
         ...state,
         cartItems: state.cartItems.filter(
-          cartItem => cartItem.id !== action.payload.id
+          cartItem => cartItem.id !== payload.id
         )
       };
     default:
